81dd7f31fa68f00da959497ed38faf2843e2c7e1
jacindaeng <jceng@uw.edu>
2019-03-10 12:23:04 -0700

    Uploading joined dataset (gender and national)

--- analysis.R
+++ analysis.R
@@ -11,10 +11,20 @@ library("tidyr")
 options(scipen = 999)
 
 gender_data<- readxl::read_xlsx("dataset/gender_wage.xlsx") %>%
-  filter(Occupation != "Total, full-time wage and salary workers", Total_Median_Weekly_Earnings != "-")
+  filter(Occupation != "Total, full-time wage and salary workers", Total_Median_Weekly_Earnings != "–", Male_Median_Weekly_Earnings != "–", Female_Median_Weekly_Earnings != "–")
+
+gender_data[[1]] <- toupper(gender_data[[1]])
 
 nation_data<- readxl::read_xlsx("dataset/nation_wage.xlsx") %>%
-  filter(OCC_GROUP == "broad") %>%
+  filter(OCC_GROUP == "broad", H_MEAN != "*") %>%
   select(OCC_TITLE, H_MEAN, A_MEAN)
 
-View(gender_data)
+colnames(nation_data)[1] <- "Occupation"
+colnames(nation_data)[2] <- "Hourly_Mean"
+colnames(nation_data)[3] <- "Annual_Mean"
+
+nation_data[[1]] <- toupper(nation_data[[1]])
+
+gender_nation_data <- inner_join(gender_data, nation_data, by = "Occupation")
+
+View(gender_nation_data)
