4c08896a8fb86bf8002ffecaca8455db8635718e
jacindaeng <jceng@uw.edu>
2019-03-10 12:57:35 -0700

    Uploading 2017 gender data

--- analysis.R
+++ analysis.R
@@ -10,7 +10,7 @@ library("dplyr")
 library("tidyr")
 options(scipen = 999)
 
-gender_data<- readxl::read_xlsx("dataset/gender_wage.xlsx") %>%
+gender_data<- readxl::read_xlsx("dataset/gender_wage_2017.xlsx") %>%
   filter(Occupation != "Total, full-time wage and salary workers", Total_Median_Weekly_Earnings != "–", Male_Median_Weekly_Earnings != "–", Female_Median_Weekly_Earnings != "–")
 
 gender_data[[1]] <- toupper(gender_data[[1]])
