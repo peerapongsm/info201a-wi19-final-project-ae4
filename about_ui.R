b088d18de507bf1086c99331beb86ebe4af84f3a
yizhenjin <sarahjin2013@gmail.com>
2019-03-12 22:35:43 -0700

    add_ui and edit gather server

--- about_ui.R
+++ about_ui.R
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-about <-
-=======
-about <- tabPanel(
+about <-  tabPanel(
   "About",
   headerPanel(
     h1("About this project", align = "center")
@@ -9,12 +6,23 @@ about <- tabPanel(
   mainPanel(
     h1("AE-4 Team members"),
     p("Jacinda Eng, Kelly Tran Ho, Yizhen(Sarah) Jin, Peerapong Saksommon"),
-    p("Women in workplace has always been a hot topic"),
+    br(),
+    br(),
+    p("These analysis showed the areas of focus that society needs to be aware of and address. 
+      As shown on gender page, there are still jobs with major gap in the number of female and male workers. 
+      The stereotype that women are not suited for labor-intensive jobs needs to be broken and treatment towards women in the more 
+      male-dominated occupations must to be improved. This will help better balance the number of workers in the natural resources
+      and construction field. Graph shows that even when there are jobs that have more female workers, the gap is not as 
+      significant as to when the occupation has more male workers. There are also obvious similarities between the jobs that have more 
+      of one gender employees. Therefore, society should continue to encourage women not only to enter the workforce, but to enter fields 
+      that are more heavily male-dominated. The pie chart shows the need to increase the level of race diversity in all occupations. More 
+      diversity in the workplace brings in a variety of perspectives and experiences which can greatly enhance a project or task."),
+    br(),
     br(),
     p("We used a lot of great data sets in working on this project:"),
     uiOutput("data1"),
     uiOutput("data2"),
-    uiOutput("data3")
+    uiOutput("data3"),
+    uiOutput("data4")
   )
 )
->>>>>>> a4edc127c5e11f3aaea4bca69dfcb3bb144dd7bf
