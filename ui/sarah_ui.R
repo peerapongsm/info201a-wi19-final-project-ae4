ad45199a367bbe62e5909dcada4897047161beab
yizhenjin <sarahjin2013@gmail.com>
2019-03-13 01:02:25 -0700

    section sarah ui

--- ui/sarah_ui.R
+++ ui/sarah_ui.R
@@ -1,65 +1,54 @@
 wage_analysis <- tabPanel(
   "Analysis",
-  tags$section(id = "map_part",
+  tags$section(id = "wage_map_part",
                h3("State GDP in the US"),
                strong(p("What is GDP?")),
-               textOutput("myGDP"),
-               leafletOutput("gdpMap")
+               textOutput("wage_GDP"),
+               leafletOutput("gdpMap"),
+               textOutput("wage_map_analysis")
                ),
-  p("Gross domestic product (GDP) is the monetary value of all the finished goods and services produced within a country's borders in a specific time period."),
-
-  p("The map below shows the GDP differences between states. The state with the highest GDP is Washington, and the state with the lowest GDP is Connecticut. 
-    Therefore, our project chose these two states and compare it with national employment data."),
   br(),
   br(),
-  tags$section(id = "first_part",
+  tags$section(id = "wage_first_part",
                h3("Occupations with the highest hourly wage differences"),
                strong(p("What occupations in Washington have the highest difference of wage comapared to the national data?")),
                plotOutput("waDiff"),
-               textOutput("analysis_1")),
-  p("The graph shows five occupations that have the greatest hourly wages difference between Washington and national wage in the year of 2017. 
-    Individuals that work as electrical drafters, emergency medical technicians and paramedics, internists,
-    or psychiatrists in state of Washington would receive pays above national level.
-    The mean difference from those five occupations are $16.374 per hour."),
+               textOutput("wage_analysis_1")),
   br(),
   br(),
-  tags$section(id = "second_part",
+  tags$section(id = "wage_second_part",
                strong(p("What occupations in Connecticut have the highest difference of wage compared to the national data?")),
                plotOutput("ctDiff"),
-               textOutput("analysis_2")
+               textOutput("wage_analysis_2")
                ),
  
-  p("Similar to the graph above, it shows the top five occupations in the state of Connecticut. Individuals that work as dentists, internists, judges, funeral Director, 
-    and real estate broker have hourly wages that exceed national averages with the highest differences. 
-    It have the mean of $16.37 above national wages from those top five occupations. Internists have the highest difference with 25.41 above national hourly wage."),
   br(),
   br(),
-  tags$section(id = "third_part",
+  tags$section(id = "wage_third_part",
                h3("Employment number in WA and CT"),
-               textOutput("analysis_3")
+               textOutput("wage_analysis_3")
                ),
-  p("States often times have their focused industries. 
-    When individuals start looking for jobs, it is good for them to keep in mind what the state is focusing on. 
-    There are always questions from new graduates to think if focused industries would have a higher demand in employment."),
-  tags$section(id = "third_part",
+  tags$section(id = "wage_third_part",
                strong(p("What job has the highest number of employees in Washington?")),
                plotOutput("employmentWA"),
                p("Washington state have the highest employment number in cashiers, combined food prep worker, 
-    laborers and freight, office clerks, and retail salespersons."
-  ),
+                  laborers and freight, office clerks, and retail salespersons.")),
   br(),
   br(),
-  tags$section(id = "four_part"
+  tags$section(id = "wage_four_part",
+               strong(p("What job has the highest number of employees in Connecticut?")),
+               plotOutput("employmentCT"),
+               p("In comparison, Connecticut has the highest employment number in cashier, customer service representatives, general manager,
+                registered nurses, and retail salespersons.")
+               ),
+
+  br(),
+  tags$section(id = "wage_conclusion",
+               h3("Conclusion"),
+               p("Overall, we can see that internists have higher hourly wages from both Washington and Connecticut. 
+                  Cashiers have the top five demand for employment in both of the states. 
+                 There is a similarity in high demand jobs in both states: They often times are jobs that
+                 requires less certificate and is easier to pick up. We were looking for the correlation between GDP and jobs that they are favoring.
+                 However, there is not an obvious trend in jobs that they are looking at.")
                )
-  strong(p("What job has the highest number of employees in Connecticut?")),
-  plotOutput("employmentCT"),
-  p("In comparison, Connecticut has the highest employment number in cashier, customer service representatives, general manager,
-    registered nurses, and retail salespersons."),
-  br(),
-  h3("Conclusion"),
-  p("Overall, we can see that internists have higher hourly wages from both Washington and Connecticut. 
-    Cashiers have the top five demand for employment in both of the states. 
-    There is a similarity in high demand jobs in both states: They often times are jobs that
-    requires less certificate and is easier to pick up. We were looking for the correlation between GDP and jobs that they are favoring.
-    However, there is not an obvious trend in jobs that they are looking at.")
   )
