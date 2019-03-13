ff9ad1d68c68028433f90d7385337b04f4a669c6
peeras-1766951 <peeras@uw.edu>
2019-03-13 13:20:58 -0700

    fix about

--- ui/about_ui.R
+++ ui/about_ui.R
@@ -14,10 +14,15 @@ about <-  tabPanel(
       that are more heavily male-dominated. The pie chart shows the need to increase the level of race diversity in all occupations. More 
       diversity in the workplace brings in a variety of perspectives and experiences which can greatly enhance a project or task."),
     br(),
-    p("We used a lot of great data sets in working on this project:"),
-    p(tagList("- ", a("Labor Force Statistics from the Current Population", href = "https://www.bls.gov/cps/cpsaat39.htm"))),
-    p(tagList("- ", a("National Occupational wages and employment server/dataset", href = "https://www.bls.gov/oes/current/oes_nat.htm?fbclid=IwAR1bsqWqpedgsKlPKyt-FVu8KBPmyARXdAZL-B7lt0_CQDXU-yBER4l8AlM"))),
-    p(tagList("- ", a("States Occupational wages and employment server/dataset", href = "https://www.bls.gov/oes/current/oessrcst.htm?fbclid=IwAR3S4-BpXYofas6If42fOReztuCYdWgVq24MCXFLS7KU6a0BrJk7h7Zg5bQ"))),
-    p(tagList("- ", a("Where are all women?", href = "https://www.theguardian.com/careers/careers-blog/2015/may/19/where-are-all-the-women-why-99-of-construction-site-workers-are-male")))
+    p("We used a lot of great data sets in working on this project:", br(),
+    tagList("- ", a("Labor Force Statistics from the Current Population", 
+                    href = "https://www.bls.gov/cps/cpsaat39.htm")), br(),
+    tagList("- ", a("National Occupational wages and employment server/dataset", 
+                    href = "https://www.bls.gov/oes/current/oes_nat.htm?fbclid=IwAR1bsqWqpedgsKlPKyt-FVu8KBPmyARXdAZL-B7lt0_CQDXU-yBER4l8AlM")), br(),
+    tagList("- ", a("States Occupational wages and employment server/dataset", 
+                    href = "https://www.bls.gov/oes/current/oessrcst.htm?fbclid=IwAR3S4-BpXYofas6If42fOReztuCYdWgVq24MCXFLS7KU6a0BrJk7h7Zg5bQ")), br(),
+    tagList("- ", a("Where are all women?", 
+                    href = "https://www.theguardian.com/careers/careers-blog/2015/may/19/where-are-all-the-women-why-99-of-construction-site-workers-are-male"))
     )
   )
+)
