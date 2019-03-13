c377d86520024eeee909b8842ffd2fb2932eff1e
jacindaeng <jceng@uw.edu>
2019-03-13 13:13:41 -0700

    Centering home page

--- server/gather_server.R
+++ server/gather_server.R
@@ -662,31 +662,4 @@ gather_server <- function(input, output) {
     }
     filter_data %>% head(10)
   })
-  # All text outputs for the home_ui page
-  output$page_title_home_ui <- renderText({
-    "US Occupational Statistics"
-  })
-
-  output$project_description_home_ui <- renderText({
-    "Our project studies the distribution of wages among occupations. 
-      Since the wage varies from each states, we chose states with the highest and lowest GDP and compare them with nation wages. In this case for 2017, the state with the highest GDP is Washington, and the state with the lowest GDP is Connecticut. Comparing these states will help us develop a deeper understanding regarding each states' focus within their respective economy. For example, if there is a higher wage towards technology related fields in Washington compared to Connecticut, then there is a trend that Washington has a stronger focus towards technology related careers."
-  })
-
-  output$page_description_home_ui <- renderText({
-    "Page Description"
-  })
-
-  output$wage_description_home_ui <- renderText({
-    "The 'Wage' tab provides a description of the states' GDP in a map format. It also describes how GDP affect's each states occupation's wage and total number of employees relating to that."
-  })
-
-  output$gender_description_home_ui <- renderText({
-    "The 'Gender' tab describes the distribution of occupations across men and women and across races as well.
-      Looking at this data can help us understand how gender, occupations, wage ties together in a subtle way. In other words, if we see that there are more men have an occupation with a higher wage, then 
-due to the lack of diversity within that occupation, there is a large wage gap between men and women."
-  })
-
-  output$about_home_ui <- renderText({
-    "The 'About' tab contains references of the datasets."
-  })
 }
