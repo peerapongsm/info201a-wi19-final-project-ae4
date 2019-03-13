e29b71feab426ac9637538b383706b10ec9cbcca
peeras-1766951 <peeras@uw.edu>
2019-03-12 22:33:05 -0700

    fix path

--- gather_app.R
+++ gather_app.R
@@ -7,8 +7,8 @@ library("geojsonio")
 library("stringr")
 library("DT")
 library("readxl")
-source("gather_ui.R")
-source("gather_server.R")
+source("ui/gather_ui.R")
+source("server/gather_server.R")
 
 ## Create a shiny app
 shinyApp(ui = gather_ui, server = gather_server)
